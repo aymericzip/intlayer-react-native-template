6d3a8699e3f71717de9f9f8ac3714cf92e180fe0
Aymeric PINEAU <ay.pineau@gmail.com>
2025-05-21 13:28:29 +0200

    core: update package

--- intlayer.config.ts
+++ intlayer.config.ts
@@ -4,8 +4,7 @@ const config: IntlayerConfig = {
   internationalization: {
     locales: [
       Locales.ENGLISH,
-      Locales.FRENCH,
-      Locales.SPANISH,
+      Locales.RUSSIAN,
       // ... Add any other locales you need
     ],
     defaultLocale: Locales.ENGLISH,
