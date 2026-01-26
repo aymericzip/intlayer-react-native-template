25ba09c1bd998fc38aed207bc1347613228612d2
Aymeric PINEAU <ay.pineau@gmail.com>
2026-01-26 17:07:57 +0100

    chore: update .gitignore to include lock files and modify intlayer dependencies to latest version

--- intlayer.config.ts
+++ intlayer.config.ts
@@ -3,11 +3,11 @@ import { type IntlayerConfig, Locales } from "intlayer";
 export const locales = [Locales.ENGLISH, Locales.FRENCH, Locales.SPANISH];
 
 const config: IntlayerConfig = {
-	internationalization: {
-		locales,
-		defaultLocale: Locales.ENGLISH,
-		strictMode: "strict",
-	},
+  internationalization: {
+    locales,
+    defaultLocale: Locales.ENGLISH,
+    strictMode: "strict",
+  },
 };
 
 export default config;
